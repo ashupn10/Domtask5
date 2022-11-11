aa3f04241a95077e4e6072d4676a35abbc7c8764
ashupn10 <68610225+ashupn10@users.noreply.github.com>
2022-11-11 22:29:22 +0530

    Dom task 5

---
+++ index.js
@@ -0,0 +1,17 @@
+// let listItmbyclass=document.getElementsByClassName('list-group-item');
+
+// for(let i=0;i<listItmbyclass.length;i++){
+//     listItmbyclass[i].style.fontWeight='bold';
+//     listItmbyclass[i].style.color='white';
+//     listItmbyclass[i].style.backgroundColor='black';
+// }
+
+
+
+let listItmbyclass=document.getElementsByTagName('li');
+
+for(let i=0;i<listItmbyclass.length;i++){
+    listItmbyclass[i].style.fontWeight='bold';
+    listItmbyclass[i].style.color='white';
+    listItmbyclass[i].style.backgroundColor='black';
+}
