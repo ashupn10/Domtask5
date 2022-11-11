3cd9f7c293337402e052b238781c4c6c05471f36
ashupn10 <68610225+ashupn10@users.noreply.github.com>
2022-11-11 22:47:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let items = document.querySelectorAll('.list-group-item');
-items[1].style.backgroundColor='green';
-items[2].style.display='none';
+items[1].style.color='green';
+for(let i=0;i<items.length;i++){
+    if(i%2==0){
+        items[i].style.backgroundColor='green';
+    }
+}
 
-
-console.log(items);
